6cb8eb0e6fa46d92e5fecf8e679bd5c16935932a
Tobi <tobiedeh@gmail.com>
2022-11-15 06:00:25 -0600

    Finished the design. Added a vertical version. Added 3 button one to change the horizonal size the other to change the vertical size and the last one to reset.

--- etch.js
+++ etch.js
@@ -1,18 +1,73 @@
 const container = document.getElementById('container')
 const size = document.getElementById('size')
+const theHeader = document.getElementById('header')
+const reset = document.getElementById('reset')
+const vertical = document.getElementById('vertical')
 function colorRandomier(){
     let a = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     let c = Math.floor(Math.random() * 256);
     return "rgb(" + a + ", " + b + ", " + c + ")";
 }
-for (let i = 0; i < 256; i++) {
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+for (let i = 0; i < 16 * 16; i++) {
     let box = document.createElement('div');
     // box.textContent = i;
     box.setAttribute('id','box' + i)
     box.style.cssText = "background-color: white"
     container.appendChild(box);
+    container.style.cssText = `
+    display: grid; 
+    grid-template-columns: repeat(` + 16 + `, auto);
+    grid-template-rows: repeat(` + 16 + `, auto);
+    `;
 }
+theHeader.addEventListener('mouseover', function(e){
+    if (e.target.id != 'header'){
+        let spanId = document.getElementById(e.target.id)
+        console.log(typeof(spanId));
+        spanId.style.color = colorRandomier();
+    };
+});
+reset.addEventListener('click', function(){
+    let allDiv = document.querySelectorAll('div')
+    allDiv = Array.from(allDiv)
+    for (div in allDiv) {
+        let divToWhite = document.getElementById('box' + div)
+        divToWhite.style.backgroundColor = "white";
+
+    }
+});
+vertical.addEventListener('click', function(){
+    let verticalSize = prompt('Enter a number less than or equal to 1225')
+    verticalSize = Number(verticalSize)
+    console.log(verticalSize)
+    if (verticalSize > 1225) {
+        alert("Error! Please Enter a number less than or equal to 1225")
+    } else  if (isNaN(verticalSize)) {
+        alert("Error! Make Sure You Enter A Number")
+    }
+    if (verticalSize <= 1225) {
+        removeChildNodes(container)
+        for (let i = 0; i < verticalSize; i++) {
+            let box = document.createElement('div');
+            box.setAttribute('id','box' + i)
+            box.style.cssText = "background-color: white"
+            container.appendChild(box);
+            container.style.cssText = `
+            display: grid; 
+            grid-template-columns: repeat(` + verticalSize + `);
+            grid-template-rows: repeat(` + verticalSize + `);
+            `;
+        };
+    };
+})
+
+
 
 
 
@@ -30,9 +85,22 @@ size.addEventListener('click', function(){
     } else  if (isNaN(newSize)) {
         alert("Error! Make Sure You Enter A Number")
     }
+    if (newSize <= 100) {
+        removeChildNodes(container)
+        for (let i = 0; i < newSize * newSize; i++) {
+            let box = document.createElement('div');
+            // box.textContent = i;
+            box.setAttribute('id','box' + i)
+            box.style.cssText = "background-color: white"
+            container.appendChild(box);
+            container.style.cssText = `
+            display: grid; 
+            grid-template-columns: repeat(` + newSize + `, auto);
+            grid-template-rows: repeat(` + newSize + `, auto);
+            `;
+        };
+    }
     
 })
-// container.style.cssText = 
-// 'display: grid; grid-template-columns: repeat(' + newSize + ' ,';
 
 
