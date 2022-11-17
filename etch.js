0079b2b49e841402941ab4cb21b5f6503c201d84
Tobi <tobiedeh@gmail.com>
2022-11-17 06:19:59 -0600

    Added a slider that changes the size of the grid. Added an input to accept user input for size.

--- etch.js
+++ etch.js
@@ -1,8 +1,10 @@
 const container = document.getElementById('container')
 const size = document.getElementById('size')
+const sizeText = document.getElementById('size-text')
 const theHeader = document.getElementById('header')
 const reset = document.getElementById('reset')
 const vertical = document.getElementById('vertical')
+const horizontalSize = document.getElementsByClassName('horizontal-size')
 function colorRandomier(){
     let a = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
@@ -13,6 +15,28 @@ function removeChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+}
+function sizeSlider() {
+    let rangeValue = size.value;
+    let allDiv = document.querySelectorAll('div')
+    allDiv = Array.from(allDiv)
+    for (div in allDiv) {
+        let divToWhite = document.getElementById('box' + div);
+        divToWhite.style.backgroundColor = "white";
+    };
+    removeChildNodes(container)
+    for (let i = 0; i < rangeValue * rangeValue; i++) {
+        let box = document.createElement('div');
+        // box.textContent = i;
+        box.setAttribute('id','box' + i)
+        box.style.cssText = "background-color: white"
+        container.appendChild(box);
+        container.style.cssText = `
+        display: grid; 
+        grid-template-columns: repeat(` + rangeValue + `, auto);
+        grid-template-rows: repeat(` + rangeValue + `, auto);
+        `;
+    };
 }
 for (let i = 0; i < 16 * 16; i++) {
     let box = document.createElement('div');
@@ -67,40 +91,49 @@ vertical.addEventListener('click', function(){
     };
 })
 
-
-
-
-
 container.addEventListener('mouseover', function(e){
     let boxId = document.getElementById(e.target.id)
     console.log(typeof(boxId))
     boxId.style.backgroundColor = colorRandomier();
 });
-size.addEventListener('click', function(){
-    let newSize = prompt('Enter a number less than 100')
-    newSize = Number(newSize)
-    console.log(newSize)
-    if (newSize > 100) {
-        alert("Error! Please Enter a number less than 100")
-    } else  if (isNaN(newSize)) {
-        alert("Error! Make Sure You Enter A Number")
-    }
-    if (newSize <= 100) {
-        removeChildNodes(container)
-        for (let i = 0; i < newSize * newSize; i++) {
-            let box = document.createElement('div');
-            // box.textContent = i;
-            box.setAttribute('id','box' + i)
-            box.style.cssText = "background-color: white"
-            container.appendChild(box);
-            container.style.cssText = `
-            display: grid; 
-            grid-template-columns: repeat(` + newSize + `, auto);
-            grid-template-rows: repeat(` + newSize + `, auto);
-            `;
+sizeText.textContent = size.value;
+
+for (let i = 0; i < horizontalSize.length; i++){
+    horizontalSize[i].addEventListener('click', function(e){
+        if (e.target && e.target.matches('span#size-text')) {
+            const sizeInput = document.createElement('input')
+            sizeInput.setAttribute('id', 'size-input')
+            sizeInput.setAttribute('placeholder','Type Value')
+            sizeInput.style.cssText = 'background-color: black; color: white; border: 0'
+            size.replaceWith(sizeInput)
+            console.log(sizeInput)
         };
-    }
-    
-})
+        size.oninput = function() {
+            sizeText.textContent = this.value
+        }
+    });
+};
+for (let i = 0; i < horizontalSize.length; i++){
+    horizontalSize[i].addEventListener('keydown', function(e) {
+        if (e.target && e.target.matches('input#size-input') && e.keyCode == 13) {
+            e.stopPropagation()
+            let listSize = Array.from(horizontalSize[i].childNodes)
+            let sizeInputValue = listSize[1].value
+            console.log(sizeInputValue)
+            if (isNaN(sizeInputValue)) {
+                console.log(size.value)
+                listSize[1].replaceWith(size)
+            } else {
+                listSize[1].replaceWith(size)
+                size.value = listSize[1].value
+                sizeText.textContent = size.value
+                sizeSlider()
+            }
+        }
+    })
+};
+size.onchange = function() {
+    sizeSlider();
+}
 
 
